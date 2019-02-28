08f2b893c9479368fea8b5d8ea7a45b3eddceec4
dwl <666666qq.com>
2019-02-28 21:31:26 +0800

    day-2

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
-const  express = require('express');
+const express = require('express');
 const app = express();
-// const Router = require('./api/routers');
+const Router = require('./api/routers');
 // 静态服务器
 app.use(express.static('./public'));
 // 路由接口,主路由
-// app.use('/api',Router);
-
-app.listen(1811,()=>{
+app.use('/api', Router);
+app.listen(1811, () => {
     console.log('server is running on http://localhost:1811');
 });

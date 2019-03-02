5c269ca829c757e7248ce7f84f6514ea8101ff79
mi-yo-w <1305279714@qq.com>
2019-03-02 17:45:23 +0800

    登录跳转功能

--- src/api/routers/index.js
+++ src/api/routers/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const Router = express.Router();
 const listRouter = require('./order-list');
 const delRouter= require('./del-list');
+const loginRouter = require('./login')
 
 Router.use('/order-list', listRouter);//查询接口
 Router.use('/del-list',delRouter);//删除接口
+Router.use('/login',loginRouter);//登录接口
 
 module.exports = Router;
---
+++ src/api/routers/login.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const Router = express.Router();
+const db = require('../db');
+
+Router.get('/', async (req, res) => {
+    console.log(123,req.query);
+
+    let data = await db.find('user',req.query);
+    console.log(456,data);
+     
+    var res1 = {
+        code: 0,
+        data: data
+    }
+    console.log(res1);
+    res.send(res1);
+})
+module.exports = Router;

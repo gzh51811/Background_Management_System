43df0d355edf4f58836f73cec429b7a6fb790102
mi-yo-w <1305279714@qq.com>
2019-03-05 11:05:28 +0800

    xiugai

--- src/api/routers/login.js
+++ src/api/routers/login.js
@@ -3,16 +3,16 @@ const Router = express.Router();
 const db = require('../db');
 
 Router.get('/', async (req, res) => {
-    console.log(123,req.query);
+    // console.log(123,req.query);
 
     let data = await db.find('user',req.query);
-    console.log(456,data);
+    // console.log(456,data);
      
     var res1 = {
         code: 0,
         data: data
     }
-    console.log(res1);
+    // console.log(res1);
     res.send(res1);
 })
 module.exports = Router;
